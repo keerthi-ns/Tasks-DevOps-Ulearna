694173dc40e56ef854f868555404d2a25b157d48
keerthi-ns <keerthins95@gmail.com>
2024-10-24 13:03:59 +0400

    added infra files

---
+++ infra/main.tf
@@ -0,0 +1,168 @@
+#VPC
+resource "aws_vpc" "main" {
+  cidr_block = "10.0.0.0/16"
+  enable_dns_hostnames = true
+  enable_dns_support = true
+  assign_generated_ipv6_cidr_block = true
+  tags = { Name = "terra_vpc" }
+}
+
+#Public Subnets
+resource "aws_subnet" "public_subnet_1" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = "10.0.1.0/24"
+  availability_zone       = "us-east-1b"
+  map_public_ip_on_launch = true
+}
+
+resource "aws_subnet" "public_subnet_2" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = "10.0.2.0/24"
+  availability_zone       = "us-east-1c"
+  map_public_ip_on_launch = true
+}
+
+#Private Subnets
+resource "aws_subnet" "private_subnet_1" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = "10.0.3.0/24"
+  availability_zone       = "us-east-1d"
+  map_public_ip_on_launch = false
+}
+
+resource "aws_subnet" "private_subnet_2" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = "10.0.4.0/24"
+  availability_zone       = "us-east-1e"
+  map_public_ip_on_launch = false
+}
+
+#internet gateway
+resource "aws_internet_gateway" "internet_gw" {
+  vpc_id = aws_vpc.main.id
+  tags   = { Name = "terra_igw" }
+}
+
+#Public route table
+resource "aws_route_table" "public_route_table" {
+  vpc_id = aws_vpc.main.id
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.internet_gw.id
+  }
+  tags = { Name = "terra_public_route" }
+}
+
+#Associate public subnets with route table
+resource "aws_route_table_association" "public_subnet_1_association" {
+  subnet_id      = aws_subnet.public_subnet_1.id
+  route_table_id = aws_route_table.public_route_table.id
+}
+
+resource "aws_route_table_association" "public_subnet_2_association" {
+  subnet_id      = aws_subnet.public_subnet_2.id
+  route_table_id = aws_route_table.public_route_table.id
+}
+
+# NAT Gateway for Private Subnets
+resource "aws_eip" "nat_eip" {
+  vpc = true
+}
+
+resource "aws_nat_gateway" "nat_gw" {
+  allocation_id = aws_eip.nat_eip.id
+  subnet_id     = aws_subnet.public_subnet_1.id
+}
+
+# Private Route Table
+resource "aws_route_table" "private_route_table" {
+  vpc_id = aws_vpc.main.id
+  route {
+    cidr_block     = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.nat_gw.id
+  }
+  tags = { Name = "terra_private_route" }
+}
+
+#Associate Private Subnets with Route Table
+resource "aws_route_table_association" "private_subnet_1_association" {
+  subnet_id      = aws_subnet.private_subnet_1.id
+  route_table_id = aws_route_table.private_route_table.id
+}
+
+resource "aws_route_table_association" "private_subnet_2_association" {
+  subnet_id      = aws_subnet.private_subnet_2.id
+  route_table_id = aws_route_table.private_route_table.id
+}
+
+# Security groups for bastion
+resource "aws_security_group" "bastion_sg" {
+  vpc_id = aws_vpc.main.id
+  name   = "bastion_sg" 
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  tags = {   Name = "Bastion-SG"  }
+}
+
+#Security Group for LB
+resource "aws_security_group" "lb_sg" {
+  vpc_id = aws_vpc.main.id
+  name   = "lb_sg" 
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  tags = {    Name = "LoadBalancer-SG"  }
+}
+
+# Security Group for EKS
+resource "aws_security_group" "eks_sg" {
+  vpc_id = aws_vpc.main.id
+  name   = "eks_sg"
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  tags = {    Name = "EKS-SG"  }
+}
+
+# Security Group for RDS
+resource "aws_security_group" "rds_sg" {
+  vpc_id = aws_vpc.main.id
+  name   = "rds_sg"  
+  ingress {
+    from_port   = 3306
+    to_port     = 3306
+    protocol    = "tcp"
+    cidr_blocks = ["10.0.0.0/16"]
+  }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  tags = {    Name = "RDS-SG"  }
+}
+
+
