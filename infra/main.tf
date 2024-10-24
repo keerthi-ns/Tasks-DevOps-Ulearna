570d9b6a9bfa17c8156bf33608ce7df8f9d78b64
keerthi-ns <keerthins95@gmail.com>
2024-10-24 13:50:04 +0400

    updated rules in security grp

--- infra/main.tf
+++ infra/main.tf
@@ -154,6 +154,12 @@ resource "aws_security_group" "lb_sg" {
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
   egress {
     from_port   = 0
     to_port     = 0
@@ -167,6 +173,12 @@ resource "aws_security_group" "lb_sg" {
 resource "aws_security_group" "eks_sg" {
   vpc_id = aws_vpc.main.id
   name   = "eks_sg"
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    security_groups = [aws_security_group.lb_sg.id]
+  }
   egress {
     from_port   = 0
     to_port     = 0
@@ -181,10 +193,10 @@ resource "aws_security_group" "rds_sg" {
   vpc_id = aws_vpc.main.id
   name   = "rds_sg"  
   ingress {
-    from_port   = 3306
-    to_port     = 3306
+    from_port   = 5432
+    to_port     = 5432
     protocol    = "tcp"
-    cidr_blocks = ["10.0.0.0/16"]
+    security_groups =  [aws_security_group.eks_sg.id]
   }
   egress {
     from_port   = 0
