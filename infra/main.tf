1e9e0eef259e12ff61ef32f93f4de9f1a1e23d50
keerthi-ns <keerthins95@gmail.com>
2024-10-24 16:05:37 +0400

    updated load balancer config

--- infra/main.tf
+++ infra/main.tf
@@ -361,3 +361,27 @@ resource "aws_iam_role_policy_attachment" "eks_fargate_policy" {
   role       = aws_iam_role.eks_fargate_role.name
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSFargatePodExecutionRolePolicy"
 }
+
+# Target Group for EKS Services
+resource "aws_lb_target_group" "eks_target_group" {
+  name     = "eks-target-group"
+  port     = 80
+  protocol = "HTTP"
+  vpc_id   = aws_vpc.main.id
+  health_check {
+    healthy_threshold   = 3
+    interval            = 30
+    timeout             = 5
+    unhealthy_threshold = 2
+  }
+}
+
+resource "aws_lb_listener" "http_listener" {
+  load_balancer_arn = aws_lb.main_lb.arn
+  port              = 80
+  protocol          = "HTTP"
+  default_action {
+    type = "forward"
+    target_group_arn = aws_lb_target_group.eks_target_group.arn
+  }
+}
